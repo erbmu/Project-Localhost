541a82c7fabaf0964e0803412834545e8cf6cb4d
adityabchickmath <aditya.chickmath@gmail.com>
2024-11-14 19:30:26 -0500

    app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import HomePage from './pages/Home';
+import HomePage from './pages/HomePage';
 import Login from './pages/Login';
 import Profile from './pages/Profile';
 
